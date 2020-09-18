acb0bfda9c8d3c3a04561d9f9ed2f8f5e01aaa4c
10640113 <10640113@LTILPUN21900009.lntinfotech.com>
2020-09-19 00:37:54 +0530

    Fantasy Cricket Selection.

---
+++ src/main/java/com/kirosys/model/PlayerDetailsInfo.java
@@ -0,0 +1,50 @@
+package com.kirosys.model;
+
+public class PlayerDetailsInfo {
+
+	String id;
+	String teamName;
+	String playerName;
+	String playerType;
+	String playerPoint;
+	String selectionPercent;
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getTeamName() {
+		return teamName;
+	}
+	public void setTeamName(String teamName) {
+		this.teamName = teamName;
+	}
+	public String getPlayerName() {
+		return playerName;
+	}
+	public void setPlayerName(String playerName) {
+		this.playerName = playerName;
+	}
+	public String getPlayerType() {
+		return playerType;
+	}
+	public void setPlayerType(String playerType) {
+		this.playerType = playerType;
+	}
+	public String getPlayerPoint() {
+		return playerPoint;
+	}
+	public void setPlayerPoint(String playerPoint) {
+		this.playerPoint = playerPoint;
+	}
+	public String getSelectionPercent() {
+		return selectionPercent;
+	}
+	public void setSelectionPercent(String selectionPercent) {
+		this.selectionPercent = selectionPercent;
+	}
+	
+}
